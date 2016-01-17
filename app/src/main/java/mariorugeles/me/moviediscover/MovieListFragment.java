0fc35e93dd49d3921290a6a38099cb0013dd9f1d
mrugeles <mrugeles@gmail.com>
2016-01-16 21:44:34 -0500

    Implementing call to moviedb API

--- app/src/main/java/mariorugeles/me/moviediscover/MovieListFragment.java
+++ app/src/main/java/mariorugeles/me/moviediscover/MovieListFragment.java
@@ -8,6 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,12 +20,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+
+import mariorugeles.me.moviediscover.model.Movie;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieListFragment extends Fragment {
     private final String LOG_TAG = MovieListFragment.class.getSimpleName();
+
+    ArrayAdapter<Movie> movieListAdapter;
 
     public MovieListFragment() {
     }
@@ -35,6 +45,12 @@ public class MovieListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.v(LOG_TAG, "MovieListFragment Ok");
+        movieListAdapter = new ArrayAdapter<Movie>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                new ArrayList<String>()
+        );
         return inflater.inflate(R.layout.movielistfragment, container, false);
     }
 
@@ -42,12 +58,12 @@ public class MovieListFragment extends Fragment {
         new FetchMoviesTask().execute();
     }
 
-    public class FetchMoviesTask extends AsyncTask<Void, Void, Void> {
+    public class FetchMoviesTask extends AsyncTask<Void, Void, Movie[]> {
 
         private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Movie[] doInBackground(Void... params) {
             Log.v(LOG_TAG, "FetchMoviesTask Ok");
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
@@ -102,11 +118,46 @@ public class MovieListFragment extends Fragment {
                     try {
                         reader.close();
                     } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                        Log.e("MovieListFragment", "Error closing stream", e);
                     }
                 }
             }
+
+            try {
+                return getMovieListFromJson(jsonResponseStr);
+            } catch (JSONException e) {
+               Log.e(LOG_TAG, "JSON Error", e);
+            }
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Movie[] result){
+            Log.v(LOG_TAG, "onPostExecute");
+            if(result != null){
+                movieListAdapter.clear();
+                for (Movie movie:result ) {
+                    Log.v(LOG_TAG, movie.getTitle());
+                    movieListAdapter.add(movie);
+                }
+            }
+        }
+
+        private Movie[] getMovieListFromJson(String jsonResponseStr) throws JSONException {
+            JSONObject jsonResponse = new JSONObject(jsonResponseStr);
+            JSONArray movieListJSONArray = jsonResponse.getJSONArray("results");
+            ArrayList<Movie> movieList = new ArrayList<Movie>();
+            for(int i = 0; i < movieListJSONArray.length(); i++) {
+                JSONObject movieJSON = movieListJSONArray.getJSONObject(i);
+                Movie movie = new Movie();
+                movie.setTitle(movieJSON.getString("original_title"));
+                movie.setPoster(movieJSON.getString("poster_path"));
+                movie.setSynopsis(movieJSON.getString("overview"));
+                movie.setRating(new Double(movieJSON.getDouble("vote_average")).floatValue());
+                movie.setReleaseDate(movieJSON.getString("release_date"));
+                movieList.add(movie);
+            }
+            return movieList.toArray(new Movie[movieList.size()]);
         }
     }
 }
---
+++ app/src/main/java/mariorugeles/me/moviediscover/model/Movie.java
@@ -0,0 +1,53 @@
+package mariorugeles.me.moviediscover.model;
+
+/**
+ * Created by mario on 1/16/16.
+ */
+public class Movie {
+    private String title;
+    private String poster;
+    private String synopsis;
+    private float rating;
+    private String releaseDate;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
